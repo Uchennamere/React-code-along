328d8d745f372804e738053fae4fb3db26c4d468
Uchennamere <agsms93agams@gmail.com>
2020-02-06 16:15:12 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -48,14 +48,19 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Logo from './map.gif';
 // We can write a javasccript code in react and parse it using {}
 //  const welcome = "Welcome to React";
 
 class App extends Component {
   state = {
-    toggle: true
+    toggle: true,
+    newText: "This is the new text",
+    backgroundColor: "#282c34",
+    newLogo: logo
   }
+
+  
 
   toggle = () => {
     this.setState({
@@ -63,14 +68,38 @@ class App extends Component {
     })
   }
   
+  newText =()=> {
+    this.setState({
+      newText: !this.state.newText
+    })
+  }
+//changecolor function using ternary operat0r, this reutrns true or false. the ? 
+//chcks if the condition if true and the : let us toggle btw the two condition
+  changeColor = () => {
+    this.state.backgroundColor === "#282c34" ?
+    this.setState({backgroundColor: 'red' }) :
+    this.setState({backgroundColor: "#282c34"})
+  }
+
+  changeLogo =()=> {
+    this.state.newLogo === logo ?
+    this.setState({newLogo:Logo }):
+    this.setState({newLogo:logo}) 
+  }
+
   render (){
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+
+      <div className="App" >
+        <header className="App-header" style={{backgroundColor: this.state.backgroundColor}}>
+          <img src={this.state.newLogo} className="App-logo" alt="logo" />
           {/* -----------------We then render the Welcome component (line 62) -------------*/}
            {/* ----in line 63, TEXT is the prop on Welcome is defined */}
           <Welcome text="Welcome to Using Props"/> 
+          {this.state.newText &&
+         <h1>THIS IS THE NEW APP</h1>
+          }
+         
           {/*The Welcome variable declared in line 53 is parsed using {}  */}
           {/* <h1 className="App-tittle">{Welcome}</h1> */}
         </header>
@@ -81,6 +110,9 @@ class App extends Component {
         <p> This should show and hide</p> 
         }
         <button onClick={this.toggle}>Show / Hide</button>
+        <button onClick={this.newText}>New Text</button>
+        <button onClick={this.changeColor}> New Color</button>
+        <button onClick={this.changeLogo}> New Logo</button>
       </div>
     );
   } 
